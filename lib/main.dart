c4e0c84ad6223f36c8d8bb4ac0e53aaa0dc1f466
2dam3 <ikbem@plaiaundi.net>
2020-11-05 09:35:16 +0100

    cambiando detalles

--- lib/main.dart
+++ lib/main.dart
@@ -99,32 +99,34 @@ class _MyHomePageState extends State<MyHomePage> {
                           padding: EdgeInsets.all(8),
                           child: Text('FB'),
                           decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              begin: Alignment.topRight,
-                              end: Alignment.bottomLeft,
-                              colors: [Colors.blue, Colors.green],
-                            ),
-                          ),
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(10)),
                         )
                       : i % 5 == 0
                           ? Container(
                               alignment: Alignment.center,
                               padding: EdgeInsets.all(8),
                               child: Text('Buck'),
-                              color: Colors.green,
+                              decoration: BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius: BorderRadius.circular(5)),
                             )
                           : i % 3 == 0
                               ? Container(
                                   alignment: Alignment.center,
                                   padding: EdgeInsets.all(8),
                                   child: Text('Face'),
-                                  color: Colors.blue,
+                                  decoration: BoxDecoration(
+                                      color: Colors.purple,
+                                      borderRadius: BorderRadius.circular(5)),
                                 )
                               : Container(
                                   alignment: Alignment.center,
                                   padding: EdgeInsets.all(8),
                                   child: Text('$i'),
-                                  color: Colors.white,
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      borderRadius: BorderRadius.circular(5)),
                                 )
               ],
             ),
