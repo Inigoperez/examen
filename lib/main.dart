9b3dfdc4abb58feed94fba8b8ecdf176606b721f
2dam3 <ikbem@plaiaundi.net>
2020-11-05 09:25:53 +0100

    ejercicio terminado

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  List<Container> containers = new List();
 
   void _incrementar() {
     setState(() {
@@ -51,24 +52,85 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(children: [
-        Row(
-          children: [
-            IconButton(
-                onPressed: () => _incrementar(),
-                icon: Icon(Icons.add),
-                color: Colors.green),
-            Text(
-              '${_counter}',
-              style: Theme.of(context).textTheme.headline3,
+      body: Center(
+        child: Column(children: [
+          Container(
+              child: Row(
+            children: [
+              FloatingActionButton(
+                onPressed: _incrementar,
+                child: Icon(
+                  Icons.add,
+                  size: 35,
+                  color: Colors.black,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 15, right: 15),
+                child: Text(
+                  "$_counter",
+                  style: TextStyle(
+                    fontSize: 40,
+                  ),
+                ),
+              ),
+              FloatingActionButton(
+                onPressed: _reducir,
+                child: Icon(
+                  Icons.remove,
+                  size: 35,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          )),
+          Container(
+            height: 400,
+            child: GridView.count(
+              padding: EdgeInsets.all(20),
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              crossAxisCount: 4,
+              children: [
+                for (var i = 1; i < _counter + 1; i++)
+                  i % 3 == 0 && i % 5 == 0
+                      ? Container(
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.all(8),
+                          child: Text('FB'),
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              begin: Alignment.topRight,
+                              end: Alignment.bottomLeft,
+                              colors: [Colors.blue, Colors.green],
+                            ),
+                          ),
+                        )
+                      : i % 5 == 0
+                          ? Container(
+                              alignment: Alignment.center,
+                              padding: EdgeInsets.all(8),
+                              child: Text('Buck'),
+                              color: Colors.green,
+                            )
+                          : i % 3 == 0
+                              ? Container(
+                                  alignment: Alignment.center,
+                                  padding: EdgeInsets.all(8),
+                                  child: Text('Face'),
+                                  color: Colors.blue,
+                                )
+                              : Container(
+                                  alignment: Alignment.center,
+                                  padding: EdgeInsets.all(8),
+                                  child: Text('$i'),
+                                  color: Colors.white,
+                                )
+              ],
             ),
-            IconButton(
-                onPressed: () => _reducir(),
-                icon: Icon(Icons.remove),
-                color: Colors.red),
-          ],
-        )
-      ]),
+          )
+        ]),
+      ),
     );
   }
 }
